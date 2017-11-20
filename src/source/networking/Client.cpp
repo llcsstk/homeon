0aa517cf98b97dcdf78389367eaa68a9c26e148c
Lucas Leite <lleitecs@gmail.com>
2017-11-20 00:50:21 -0200

    Atualizações gerais

--- src/source/networking/Client.cpp
+++ src/source/networking/Client.cpp
@@ -8,14 +8,14 @@
 
 CSClient::CSClient(tcp::socket socket)
 	//: socket_(std::move(socket))
-    : socket_(std::move(socket)), 
+    : socket_(std::move(socket)),
 	ping_thread(&CSClient::StartPingControl, this),
 	input_deadline_(socket_.get_io_service())
 {
 	cinfo = new CSClientConnectionInfo();
 	cinfo->client_id = -1;
 	cinfo->timestamp = -1;
-	
+
 	isActive = true;
 }
 
@@ -34,7 +34,7 @@ void CSClient::Start()
 void CSClient::StartRead()
 {
 	auto self(shared_from_this());
-	
+
 	boost::asio::async_read(socket_,
 	boost::asio::buffer(recv_buffer),
 	boost::asio::transfer_at_least(0x0001),
@@ -57,9 +57,9 @@ void CSClient::StartRead()
 void CSClient::SendWelcome()
 {
 	auto self(shared_from_this());
-	
+
 	Packet* welcome_packet = ServerPackets::Welcome(new Packet(cinfo));
-	
+
     boost::asio::async_write(socket_,
 	boost::asio::buffer(welcome_packet->data(),
 	welcome_packet->length()),
@@ -78,15 +78,15 @@ void CSClient::SendWelcome()
 
 void CSClient::SendPing()
 {
-	if(!isActive)
-		return;
-		
+	//if(!isActive)
+		//return;
+
 	//CLogger::GetLogger()->Log("Sending Ping Packet");
 
 	auto self(shared_from_this());
-	
+
 	Packet* welcome_packet = ServerPackets::Ping(new Packet(cinfo));
-	
+
     boost::asio::async_write(socket_,
 	boost::asio::buffer(welcome_packet->data(),
 	welcome_packet->length()),
@@ -106,9 +106,9 @@ void CSClient::SendPing()
 void CSClient::SendSimpleAction(uint8_t value)
 {
 	auto self(shared_from_this());
-	
+
 	Packet* simple_action_packet = ServerPackets::SimpleAction(new Packet(cinfo), value);
-	
+
     boost::asio::async_write(socket_,
 	boost::asio::buffer(simple_action_packet->data(),
 	simple_action_packet->length()),
@@ -128,9 +128,9 @@ void CSClient::SendSimpleAction(uint8_t value)
 void CSClient::SendSwitchLight(uint8_t index)
 {
 	auto self(shared_from_this());
-	
+
 	Packet* light_switch_packet = ServerPackets::LightSwitch(new Packet(cinfo), index);
-	
+
     boost::asio::async_write(socket_,
 	boost::asio::buffer(light_switch_packet->data(),
 	light_switch_packet->length()),
@@ -150,7 +150,7 @@ void CSClient::SendSwitchLight(uint8_t index)
 void CSClient::SendDefaultPacket(Packet* packet)
 {
 	auto self(shared_from_this());
-	
+
     boost::asio::async_write(socket_,
 	boost::asio::buffer(packet->data(),
 	packet->length()),
@@ -175,15 +175,15 @@ void CSClient::HandlePacket(std::array<char, 1024> packet)
 		{
 			uint8_t client_id_1 = ((uint8_t) packet[0x0000]);
 			uint8_t client_id_2 = ((uint8_t) packet[0x0001]);
-			
+
 			cinfo->client_id = ((uint16_t) client_id_1 << 8) | client_id_2;
-			
+
 			uint8_t moduletype = (uint8_t) packet[0x0002];
-			
+
 			//CLogger::GetLogger()->Log("Welcome Packet received from client %d", cinfo->client_id);
 			CLogger::GetLogger()->Log("	-> Client [%d] registered successfully as %d module!", cinfo->client_id, moduletype);
 			CSClient::SendWelcome();
-			
+
 			if(moduletype == DHT_MODULE)
 				background_thread = new boost::thread(boost::bind(&CSClient::temp_humidity_thread, shared_from_this()));
 			else if(moduletype == USOLO_MODULE)
@@ -198,14 +198,16 @@ void CSClient::HandlePacket(std::array<char, 1024> packet)
 		break;
 		case PING_PACKET:
 		{
+			//CLogger::GetLogger()->Log("Received Pong Packet - Increasing deadline expire time");
 			input_deadline_.expires_from_now(boost::posix_time::seconds(7));
-			
+
 			if(!deadline_active)
 			{
+				//CLogger::GetLogger()->Log("deadline activated");
 				input_deadline_.async_wait(
 					boost::bind(&CSClient::check_deadline,
 					shared_from_this(), &input_deadline_));
-				
+
 				deadline_active = true;
 			}
 		}
@@ -214,9 +216,9 @@ void CSClient::HandlePacket(std::array<char, 1024> packet)
 		{
 			uint8_t temp_data = packet[0x0005];
 			uint8_t humidity_data = packet[0x0006];
-			
+
 			CLogger::GetLogger()->Log("Temperature Packet received from client %d , %d", cinfo->client_id, temp_data, humidity_data);
-			
+
 			DATA_SHEET::GetDataSheet()->temp = temp_data;
 			DATA_SHEET::GetDataSheet()->humidity = humidity_data;
 		}
@@ -224,36 +226,36 @@ void CSClient::HandlePacket(std::array<char, 1024> packet)
 		case LDR_PACKET:
 		{
 			uint8_t data = packet[0x0005];
-			
+
 			CLogger::GetLogger()->Log("LDR Packet received from client %d width data %d", cinfo->client_id, data);
-			
+
 			DATA_SHEET::GetDataSheet()->luminosity = data;
 		}
 		break;
 		case MQ2_PACKET:
 		{
 			uint8_t data = packet[0x0005];
-			
+
 			CLogger::GetLogger()->Log("MQ2 Packet received from client %d width data %d", cinfo->client_id, data);
-			
+
 			DATA_SHEET::GetDataSheet()->mq2_presence = (data == (uint8_t) 0x0001);
 		}
 		break;
 		case MQ6_PACKET:
 		{
 			uint8_t data = packet[0x0005];
-			
+
 			CLogger::GetLogger()->Log("MQ6 Packet received from client %d width data %d", cinfo->client_id, data);
-			
+
 			DATA_SHEET::GetDataSheet()->mq6_presence = (data == (uint8_t) 0x0001);
 		}
 		break;
 		case USOLO_PACKET:
 		{
 			uint8_t data = packet[0x0005];
-			
+
 			CLogger::GetLogger()->Log("USolo Packet received from client %d width data %d", cinfo->client_id, data);
-			
+
 			DATA_SHEET::GetDataSheet()->usolo = data;
 		}
 		break;
@@ -265,8 +267,10 @@ void CSClient::HandlePacket(std::array<char, 1024> packet)
 
 void CSClient::StartPingControl()
 {
+	//CLogger::GetLogger()->Log("Starting Ping Control");
+	sleep(2);
 	while(isActive)
-	{		
+	{
 		sleep(2);
 		SendPing();
 	}
@@ -276,12 +280,12 @@ void CSClient::CloseClient()
 {
 	isActive = false;
 	deadline_active = false;
-	
+
 	input_deadline_.cancel();
-	
+
 	if(background_thread != NULL)
 		background_thread->interrupt();
-	
+
 	CLogger::GetLogger()->Log("	Closing Client [%d] ", cinfo->client_id);
 	ClientManager::GetManager()->UnregisterClient(shared_from_this());
 }
@@ -296,9 +300,11 @@ void CSClient::check_deadline(boost::asio::deadline_timer* deadline)
 	if (CSClient::disconnected())
 		return;
 
+	//CLogger::GetLogger()->Log("Checking Deadline");
+
 	if (deadline->expires_at() <= boost::asio::deadline_timer::traits_type::now())
 	{
-		CLogger::GetLogger()->Log("Deadline has been expired!");
+		//CLogger::GetLogger()->Log("Deadline has been expired!");
 		CSClient::CloseClient();
 	}
 	else
